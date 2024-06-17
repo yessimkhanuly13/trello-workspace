b2628e393c3ba374cc5046b686365548fda15b9d
yessimkhanuly13 <esimhanuli13@gmail.com>
2024-06-17 12:41:17 +0500

    home new board popover

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,39 +1,30 @@
-import { useDispatch, useSelector } from "react-redux"
+import { useSelector } from "react-redux"
 import { RootState } from "../store/store"
-import { addBoard } from "../store/board/boardSlice"
-import { useState } from "react"
 import { Link } from "react-router-dom"
 import { NavbarComp } from "./index"
 
+import NewBoardPopover from "./NewBoardPopover"
+
 function Home() {
   const boards = useSelector((state: RootState) => state.board.boards)
-  const dispatch = useDispatch()
-  const [boardTitle, setBoardTitle] = useState<string>("")
-
-  const handleBoard = () => {
-    const boardId = "" + boards.length + 1
-    console.log(boardId)
-    dispatch(addBoard({boardTitle, boardId}))
-    console.log(boards)
-  }
-
   
   return (
     <>
     <NavbarComp/>
     <div className="flex flex-col m-2 p-2">
-      <h2>Boards:</h2>
+      <h2 className="ml-2">Boards:</h2>
       <div className="flex flex-between flex-auto">
         {boards.map((board)=>{
           return(
-            <div className={`${board.backgroundColor} border m-2 flex flex-col p-2 w-1/6 h-12`}>
+            <div className={`${board.backgroundColor} w-1/6 border cursor-pointer ml-2`}>
                 {/* <button onClick={()=> dispatch(deleteBoard({ id: board.id }))}>Delete</button> */}
                 <Link to={`/board/${board.id}`}>
-                  <div>{board.title}</div>
+                  <div className="m-2 flex flex-col p-2 w-full h-16">{board.title}</div>
                 </Link>
             </div>
           )
-        })}
+        })} 
+          <NewBoardPopover placement="right" buttonStyle="h-full w-full p-2 ml-2" buttonText={`Create new board ${boards.length} remaining`}/>            
       </div>
     </div>
     </> 
---
+++ src/components/NewBoardPopover.tsx
@@ -0,0 +1,106 @@
+import React from 'react'
+import { Button, Popover, PopoverTrigger, PopoverContent, Input } from '@nextui-org/react'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addBoard } from '../store/board/boardSlice'
+import { v4 as uuidv4 } from 'uuid';
+
+interface Gradient {
+    style: string
+}
+
+const COLORS: Gradient[] = [
+    {style: "bg-gradient-to-r from-green-400 from-pink-500"},
+    {style: "bg-gradient-to-r from-green-400 to-blue-500"}, 
+    {style: "bg-gradient-to-r from-pink-500 to-yellow-500"}, 
+    {style: "bg-gradient-to-r from-indigo-500 to-yellow-500"}
+  ]
+
+function NewBoardPopover({placement, buttonStyle="", buttonText="Create"}) {
+    const [boardTitle, setBoardTitle] = useState<string>("")
+    const [isValid, setIsValid] = useState<boolean>(true)
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+    const [backgroundColor, setBackgroundColor] = useState<string>("")
+    const dispatch = useDispatch()
+  
+    const handleChange = (e) => {
+      setBoardTitle(e.target.value)
+      setIsValid(true)
+    }
+  
+  
+    const handleClick = () => {
+      if(boardTitle === ""){
+        setIsValid(false)
+        return 
+      }
+  
+      const boardId = uuidv4()
+      dispatch(addBoard({boardId,boardTitle, backgroundColor}))
+      setBoardTitle("")
+      setIsOpen(false)
+      setBackgroundColor("")
+    }
+  
+    const handleChangeBg = (value) => {
+      setBackgroundColor(value)
+      console.log(value)
+    }
+  return (
+    <div>
+        <Popover 
+            isOpen={isOpen}
+            onOpenChange={(open) => setIsOpen(open)}
+            placement={placement}
+            showArrow={true} 
+            offset={20} 
+            radius='none'>
+            <PopoverTrigger>
+              <Button radius='none' color='primary' className={buttonStyle}>{buttonText}</Button>
+            </PopoverTrigger>
+            <PopoverContent>
+            <div className='py-2'>
+                <p className='text-start'>Background:</p>
+                <div className={`mt-1 ${backgroundColor} p-2`}>
+                  <img src="https://trello.com/assets/14cda5dc635d1f13bc48.svg" alt="" />
+                </div>
+                <div className='grid grid-cols-4 grid-flow-col gap-2 mt-1 '>
+                    {
+                      COLORS.map((gradient)=>{
+                        return (
+                          <Button className={`mt-1 ${gradient.style}`} radius='sm' isIconOnly onClick={()=>handleChangeBg(gradient.style)}>
+                          </Button>
+                        )
+                      })
+                    }
+                </div>
+              </div>
+
+              <div className='p-4'>
+              <Input 
+                label='Board title' 
+                labelPlacement="outside" 
+                isRequired 
+                description="👋 Board title is required" 
+                radius='sm' 
+                placeholder='Amazing board' 
+                value={boardTitle}
+                isInvalid={!isValid}
+                onChange={(e)=>handleChange(e)}/>
+                
+              <Button 
+               onClick={handleClick} 
+               className='w-full' radius='none'
+               color='primary'
+               >
+                Create
+                </Button>
+              </div>
+
+            </PopoverContent>
+          </Popover>
+    </div>
+  )
+}
+
+export default NewBoardPopover
